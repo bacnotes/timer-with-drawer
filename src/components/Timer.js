3839fbb9f71da7191d19f7e59a531ef30413c1a4
bacnote <bacnote@example.com>
2022-01-26 23:55:37 +0800

    fix/countdown UI will not out of container

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -16,14 +16,26 @@ const Timer = () => {
   const [startTimer, setStartTimer] = useState(isCountdown);
   const [countdownTime, setCountdownTime] = useState(minute * 60);
   const { min, sec } = formatTime(countdownTime);
-  
-  
 
   // 沒有倒數時 表單連動畫面
   const timeChangeHandler = (e) => {
-    const newMinute = e.target.value;
+    const newMinute = Number(e.target.value);
+    console.log(newMinute);
+    if (Number.isNaN(newMinute)) return
     setMinute(newMinute);
     setCountdownTime(newMinute * 60);
+  };
+
+  // 限制數字
+  const onlyNumber = (e) => {
+    if (Number.isNaN(e.target.value)) {
+      Toast.fire({
+        icon: "warning",
+        title: "請輸入純數字",
+      });
+      return;
+    }
+    e.target.value = e.target.value.replace(/[^\d]/g, "");
   };
 
   // 倒數更新秒數
@@ -34,7 +46,7 @@ const Timer = () => {
         icon: "warning",
         title: "時間範圍為1~120分鐘",
       });
-       return
+      return;
     }
     if (countdownTime > 0 && startTimer) {
       setTimeout(() => {
@@ -56,12 +68,14 @@ const Timer = () => {
         <input
           placeholder='1'
           disabled={isCountdown}
+          onKeyUp={onlyNumber}
           onChange={!isCountdown ? timeChangeHandler : null}
           className='mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
           focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
-          type='number'
-          min='1'
-          max='120'
+          type='text'
+          size='3'
+          maxLength='3'
+          required
         />
         <span>分鐘</span>
         <button
@@ -72,16 +86,16 @@ const Timer = () => {
           disabled={isCountdown || minute > 120 || minute < 1 || !minute}
           type='button'
           className='bg-teal-500 hover:bg-teal-700 disabled:bg-slate-500 text-white font-bold py-2 px-4 rounded'>
-          {isCountdown? '倒數中' :'開始'}
+          {isCountdown ? "倒數中" : "開始"}
         </button>
       </div>
       <div className='mt-1 h-20 text-8xl outline-1 relative'>
         {!isCountdown ? (
-          <span className='text-sky-500 absolute right-10'>{minute}</span>
+          <span className='text-sky-500 absolute -right-2'>{minute}</span>
         ) : (
-          <span className='text-sky-500 absolute right-10'>{min}</span>
+          <span className='text-sky-500 absolute right-0'>{min}</span>
         )}
-        <span className='text-sky-600 absolute -left-3 -top-2'>:</span>
+        <span className='text-sky-600 absolute left-2 -top-2'>:</span>
         <span className='text-sky-500 absolute left-8'>{sec}</span>
       </div>
     </section>
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ export const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
   showConfirmButton: false,
-  timer: 3000,
+  timer: 1000,
 });
 
 export const getRandom = (num) => {
